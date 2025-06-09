1a8161b9c87a336708f5c167e14f03f599e00004
aluno <etecae023@hotmail.com>
2025-06-09 08:09:08 -0300

    Alterando o rules

--- test/exemple.test.js
+++ test/exemple.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('Exemplo de teste', function(){
     it("Deve somar dois números corretamente", function(){
-        assert.strictEqual(1 + 1, 7)
+        assert.strictEqual(1 + 1, 2)
    
     });
 });
